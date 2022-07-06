aa19778bbd678b431f2ef4ca15f2acb1fff4f9e2
josh.sun <joshy4werk@gmail.com>
2022-07-06 09:39:03 +0800

    first commit

---
+++ src/test/java/com/josh/practice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
