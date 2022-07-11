a3571335396a1b6c8e76005f99c140b6f59fd7f9
josh.sun <joshy4werk@gmail.com>
2022-07-11 17:32:02 +0800

    springboot jpa, addon native query

--- src/main/java/com/josh/practice/jpa/handler/MemberHandler.java
+++ src/main/java/com/josh/practice/jpa/handler/MemberHandler.java
@@ -2,9 +2,14 @@ package com.josh.practice.jpa.handler;
 
 import com.josh.practice.jpa.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberHandler extends JpaRepository<Member, Integer> {
+
+    @Query(value = "SELECT * FROM jpa_practice.members WHERE ID = :id", nativeQuery = true)
+    Member customQuery(@Param("id") Integer id);
 
 }
